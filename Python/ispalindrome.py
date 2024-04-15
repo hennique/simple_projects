85329483ae1807f5e656e4791ee763e48214f5b6
hennique <henriquevelososribeiro@gmail.com>
2024-04-14 23:46:31 -0300

    Added more comments to ispalindrome.py

--- Python/ispalindrome.py
+++ Python/ispalindrome.py
@@ -2,18 +2,21 @@ def is_palindrome(word):
     normal_word = []
     reversed_word = []
     
+    # Check each word letter and add them to a list
     for i in word:
         if i not in [" ", ",", ".", "?", "'", "!"]:
             normal_word.append(i)
 
-    # Will reverse the word
+    # Reverse the word and do the same
     for i in word[::-1]:
         if i not in [" ", ",", ".", "?", "'", "!"]:
             reversed_word.append(i)
     
+    # Join the list to a string so it can be compared
     normal_word = ("".join(normal_word)).lower()
     reversed_word = ("".join(reversed_word)).lower()
     
+    # Compare the two words
     if normal_word == reversed_word:
         return "Is a palindrome"
     else:
